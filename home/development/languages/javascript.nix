588de6d1f2f97892e20585cd3d480af05a6ec8e0
Abdellah Azarfane <abdellahazarfane@proton.me>
2025-12-27 18:15:13 +0100

    I Hate FIssssssssssssssssssssssh

--- home/development/languages/javascript.nix
+++ home/development/languages/javascript.nix
@@ -2,7 +2,7 @@
 {
   home.packages = with pkgs; [
     # --- Javascript/Typescript/React.js/Next.js ---
-    nodejs # Node.js JavaScript runtime
+    # nodejs # Node.js JavaScript runtime
     nodePackages.eslint
     nodePackages.npm
     nodePackages.prettier
