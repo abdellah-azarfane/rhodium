5fce033a384a2d4321f9fc57983c279eb43177c7
Abdellah Azarfane <abdellahazarfane@proton.me>
2026-01-03 20:37:58 +0100

    glsll error

--- home/development/languages/glsl.nix
+++ home/development/languages/glsl.nix
@@ -1,7 +1,19 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let
+  ccVersion = pkgs.stdenv.cc.version or "";
+  gccIsTooNewForGlslls = ccVersion != "" && lib.versionAtLeast ccVersion "15";
+
+  glslLspPkg =
+    if pkgs ? shader-language-server then
+      pkgs.shader-language-server
+    else if (pkgs ? glslls) && (!gccIsTooNewForGlslls) then
+      pkgs.glslls
+    else
+      null;
+in
 {
-  home.packages = with pkgs; [
+  home.packages = lib.filter (p: p != null) [
     # --- Glsl ---
-    glslls
+    glslLspPkg
   ];
 }
